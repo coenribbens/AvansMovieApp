10904882c7b02e4cd2a1ab0256f55ad14074e441
mjlpjaco <mjlp.jacobusse@student.avans.nl>
2020-03-25 14:59:35 +0100

    Ported over the HTTPGETRequest class from previous project

---
+++ app/src/main/java/com/avans/AvansMovieApp/Utilities/MakeHTTPGETRequest.java
@@ -0,0 +1,40 @@
+package com.avans.AvansMovieApp.Utilities;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MakeHTTPGETRequest extends AsyncTask<String, Integer, String> {
+
+    private HTTPGETRequestable context;
+
+    public MakeHTTPGETRequest(HTTPGETRequestable context) {
+        this.context = context;
+    }
+
+    protected String doInBackground(String... urls) {
+        String response = "";
+        try {
+            URL url = new URL(urls[0]);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            InputStream stream = conn.getInputStream();
+            Scanner s = new Scanner(stream).useDelimiter("\\A");
+            response += s.hasNext() ? s.next() : "";
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    protected void onPostExecute(String response) {
+        // do the callback
+    }
+}
