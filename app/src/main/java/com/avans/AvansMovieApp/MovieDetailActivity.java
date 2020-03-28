5936194b6610e83bb773627f91b1f0f5fa9516ed
mjlpjaco <mjlp.jacobusse@student.avans.nl>
2020-03-28 14:11:28 +0100

    oke ik doe het wel

--- app/src/main/java/com/avans/AvansMovieApp/MovieDetailActivity.java
+++ app/src/main/java/com/avans/AvansMovieApp/MovieDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -13,7 +14,6 @@ import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.avans.AvansMovieApp.Adapters.ReviewAdapter;
 import com.avans.AvansMovieApp.Model.DetailedMovie;
 import com.avans.AvansMovieApp.Model.GlobalVariables;
 import com.avans.AvansMovieApp.Model.Review;
@@ -23,14 +23,13 @@ import com.avans.AvansMovieApp.Utilities.FetchingUtilities.GetYoutubeIdFromMovie
 import com.avans.AvansMovieApp.Utilities.FetchingUtilities.MovieIdDetailedMovieConvertable;
 import com.avans.AvansMovieApp.Utilities.FetchingUtilities.MovieIdYoutubeIdConvertable;
 import com.avans.AvansMovieApp.Utilities.FetchingUtilities.MovieReviewsConvertable;
-import com.avans.AvansMovieApp.Utilities.FetchingUtilities.PostTokenAndAuthenticate;
 import com.avans.AvansMovieApp.Utilities.NeworkUtilities.HTTPRequestable;
 import com.avans.AvansMovieApp.Utilities.NeworkUtilities.MakeHTTPPOSTRequest;
 import com.bumptech.glide.Glide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MovieDetailActivity extends AppCompatActivity implements MovieIdDetailedMovieConvertable, MovieIdYoutubeIdConvertable, MovieReviewsConvertable, HTTPRequestable {
     private TextView mTitle;
@@ -54,7 +53,12 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieIdDet
     //private ReviewAdapter mReviewAdapter;
     //private ArrayList<Review> mReviewList = new ArrayList<>();
     private RatingBar mRatingBar;
+    private Button mSubmitRatingButton;
     private float mRating;
+    private int movieId;
+    private String JSONPostData;
+
+
 
     private String API_ENDPOINT = "/movie";
     private String HTTPParameters = "/%d/rating?api_key=%s&session_id=%s";
@@ -64,17 +68,14 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieIdDet
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_detail);
-        int movieId = getIntent().getIntExtra("movieid", 0);
+        movieId = getIntent().getIntExtra("movieid", 0);
         GetDetailedMovieFromMovieId getDetailedMovieFromMovieId = new GetDetailedMovieFromMovieId(movieId, this);
         getDetailedMovieFromMovieId.initializeMovieIdToDetailedMovieRequest();
-
-
 
         GetYoutubeIdFromMovieId getYoutubeIdFromMovieId = new GetYoutubeIdFromMovieId(movieId,this);
         getYoutubeIdFromMovieId.initializeMovieIdToYoutubeIdRequest();
 
         GetReviews getReviews = new GetReviews(this, movieId);
-
 
         //Assigning all the mValues with their view equivalents.
         this.mTitle = findViewById(R.id.tv_movie_detail_title);
@@ -117,21 +118,26 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieIdDet
         });
 
         mRatingBar = findViewById(R.id.rb_rating_bar);
-        mRating = mRatingBar.getRating() * 2;
+        mSubmitRatingButton = findViewById(R.id.rating_submit_button);
+        mSubmitRatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRating = mRatingBar.getRating() * 2;
+                Log.v("{{URL}}",GlobalVariables.V3_BASE_URL + API_ENDPOINT  + String.format(HTTPParameters,MovieDetailActivity.this.movieId,GlobalVariables.API_KEY_V3,GlobalVariables.SESSION_TOKEN));
+                if(mRating >= .5) {
+                    JSONPostData = String.format("{'value':%.2f}",mRating).replace(",", ".");
+                    // TODO: this currently doesn't work, this is due to the SESSION_TOKEN being broken in general. Someone, go fix this and it'll work. also check is mRating > .5
+                    MakeHTTPPOSTRequest makeReq = new MakeHTTPPOSTRequest(MovieDetailActivity.this);
+                    makeReq.execute(GlobalVariables.V3_BASE_URL + API_ENDPOINT + String.format(
+                            HTTPParameters,
+                            movieId,
+                            GlobalVariables.API_KEY_V3,
+                            GlobalVariables.SESSION_TOKEN)
+                            , JSONPostData);
+                }
+            }});
 
-        // get a token first
-        // https://api.themoviedb.org/3/movie/1/rating?api_key=b966d45d0ab662f523ce11044a9394ef
 
-        String JSONPostData = String.format("{'value':%.2f}",mRating).replace(",", ".");;
-
-
-
-        // TODO: Do this on button press
-        // TODO: this currently doesn't work, this is due to the SESSION_TOKEN being broken in general. Someone, go fix this and it'll work. also check is mRating > .5
-        MakeHTTPPOSTRequest makeReq = new MakeHTTPPOSTRequest(MovieDetailActivity.this);
-        Log.v("{{URL}}",GlobalVariables.V3_BASE_URL + API_ENDPOINT  + String.format(HTTPParameters,movieId,GlobalVariables.API_KEY_V3,GlobalVariables.SESSION_TOKEN));
-        makeReq.execute(GlobalVariables.V3_BASE_URL + API_ENDPOINT  + String.format(HTTPParameters,movieId,GlobalVariables.API_KEY_V3,GlobalVariables.SESSION_TOKEN),JSONPostData);
-        // end
     }
 
     @Override
@@ -229,7 +235,11 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieIdDet
 
     @Override
     public void ProcessHTTPResponseBody(String HTTPGETResponse) {
-        //TODO: remove all functionality from this method! Only ment for logging RN!!
+        //TODO: observe the reponse body and see if placing the review was succesful.
+            //TODO: if so,
+                //TODO: 1. clear the rating bar with mRatingBar.setRating(0F);
+                //TODO: 2. show a toast with success
+            // TODO: else, show a toast with failure
         Log.v("{{REPSO}}",HTTPGETResponse);
     }
 }
